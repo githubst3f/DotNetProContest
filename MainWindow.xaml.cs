1916cbe761569c2b4e2d2c6092c8ad10d8f32528
githubst3f <wst@ebner.cc>
2016-06-28 13:27:40 +0200

    Added ViewModelMainWindow

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -7,6 +7,16 @@ namespace DotNetProContest
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ViewModelMainWindow _viewModelMainWindow;
+        public ViewModelMainWindow ViewModelMainWindow
+        {
+            get
+            {
+                return _viewModelMainWindow ?? 
+                    (_viewModelMainWindow = new ViewModelMainWindow());
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
